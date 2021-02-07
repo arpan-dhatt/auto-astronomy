4233e693ead5c79be6edd22cc27301402631edf4
Arpan Dhatt <ary.dhatt@gmail.com>
2021-02-07 11:56:52 -0600

    add webpage checker

--- auto-astronomy/auto_astronomyApp.swift
+++ auto-astronomy/auto_astronomyApp.swift
@@ -11,7 +11,28 @@ import SwiftUI
 struct auto_astronomyApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
+            checkWebsite()
         }
     }
+    
+    func checkWebsite(completion: @escaping (Bool) -> Void ) {
+        guard let url = URL(string: "http://47.37.119.216:8080") else { return }
+
+        var request = URLRequest(url: url)
+        request.timeoutInterval = 1.0
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("\(error.localizedDescription)")
+                completion(false)
+            }
+            if let httpResponse = response as? HTTPURLResponse {
+                print("statusCode: \(httpResponse.statusCode)")
+                completion(true)
+
+            }
+        }
+        task.resume()
+    }
+
 }
