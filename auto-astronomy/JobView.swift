e135bccf7981b9ef7ecea8685b158129045997b2
Arpan Dhatt <ary.dhatt@gmail.com>
2021-02-07 09:52:04 -0600

    jobs view

--- auto-astronomy/JobView.swift
+++ auto-astronomy/JobView.swift
@@ -11,7 +11,8 @@ struct JobView: View {
     var body: some View {
         ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: false) {
             VStack {
-                JobCardView(object: "M31", image: UIImage(named: "galaxies/1")!, updates: ["Cooling Telescope", "Aligning Telescope", "Calibrating Focus", "Beginning Exposure", "Ending Exposure", "Completed Processing"], timeRemaining: 0, status: "Completed", location: "Southlake, TX", weather: "Clear")
+                Text("Jobs").font(.title)
+                JobCardView(object: "M31", image: UIImage(named: "galaxies/1")!, updates: ["Cooling Telescope"], timeRemaining: 1600, status: "Running", location: "Southlake, TX", weather: "Clear")
                 JobCardView(object: "M31", image: UIImage(named: "galaxies/1")!, updates: ["Cooling Telescope", "Aligning Telescope", "Calibrating Focus", "Beginning Exposure", "Ending Exposure", "Completed Processing"], timeRemaining: 0, status: "Completed", location: "Southlake, TX", weather: "Clear")
                 JobCardView(object: "M31", image: UIImage(named: "galaxies/1")!, updates: ["Cooling Telescope", "Aligning Telescope", "Calibrating Focus", "Beginning Exposure", "Ending Exposure", "Completed Processing"], timeRemaining: 0, status: "Completed", location: "Southlake, TX", weather: "Clear")
             }
