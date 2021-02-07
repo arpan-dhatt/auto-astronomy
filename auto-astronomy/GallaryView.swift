8968f0192649fb0ce69b542997a2a4f9645caad6
Arpan Dhatt <ary.dhatt@gmail.com>
2021-02-07 00:49:51 -0600

    added nebulae and galaxies thing

--- auto-astronomy/GallaryView.swift
+++ auto-astronomy/GallaryView.swift
@@ -9,8 +9,38 @@ import SwiftUI
 
 struct GallaryView: View {
     @ObservedObject var model: ViewModel
+    
+    @State var selection = "Galaxies"
+    @State var hideTitle = false
+    
     var body: some View {
-        GalaxyView()
+        VStack {
+            if !hideTitle {
+                HStack {
+                    Text(selection).font(.title)
+                    Spacer()
+                    Button(action: {
+                        if selection == "Galaxies" {
+                            selection = "Nebulae"
+                        } else {
+                            selection = "Galaxies"
+                        }
+                    }) {
+                        if selection == "Galaxies" {
+                            Text("Nebulae")
+                        } else {
+                            Text("Galaxies")
+                        }
+                    }
+                }.padding()
+            }
+            if selection == "Galaxies" {
+                GalaxyView(heroBinding: $hideTitle)
+            } else {
+                NebulaView(heroBinding: $hideTitle)
+            }
+        }
+        
     }
 }
 
