fc206407f23946938873c83354e25fc1cfda88e7
Arpan Dhatt <ary.dhatt@gmail.com>
2021-02-06 22:52:54 -0600

    Initial Commit

---
+++ auto-astronomy/auto_astronomyApp.swift
@@ -0,0 +1,17 @@
+//
+//  auto_astronomyApp.swift
+//  auto-astronomy
+//
+//  Created by Arpan Dhatt on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct auto_astronomyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
